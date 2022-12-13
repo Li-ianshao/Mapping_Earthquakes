15a9ea585e072a32fb85f89a24bf39e75cd3772c
poi7415778@gmail.com <ian0920365709>
2022-12-13 10:47:03 -0600

    1213

--- Simple_Map/Static/js/logic.js
+++ Simple_Map/Static/js/logic.js
@@ -9,18 +9,14 @@ console.log("working");
 //     zoom: 4
 //   });
 
-
-
-//  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-let map = L.map(mapid).setView([34.0522, -118.2437], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6214, -122.3790], 5);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
-  id: 'mapbox/dark-v10',
+  id: 'mapbox/satellite-streets-v12',
   tileSize: 512,
   zoomOffset: -1,
   accessToken: API_KEY
@@ -28,17 +24,16 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// Get data from cities.js
-let cityData = cities;
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6214, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
 
-// Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
-  console.log(city);
-  L.circleMarker(city.location,{radius:city.population/100000}).addTo(map).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>").addTo(map);
- });
-
-L.circle([34.0522, -118.2437], {
-  radius: 300,
-  color: "black",
-  fillColor: "#ffffa1"
+// Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line, {
+  color: "yellow"
 }).addTo(map);
